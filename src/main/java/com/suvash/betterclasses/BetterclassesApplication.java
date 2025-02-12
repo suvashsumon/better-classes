f0dffe5fbc1660718e7fbc0bf5c274d467499213
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-13 00:01:50 +0600

    Initial project setup

---
+++ src/main/java/com/suvash/betterclasses/BetterclassesApplication.java
@@ -0,0 +1,13 @@
+package com.suvash.betterclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterclassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterclassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suvash/betterclasses/BetterclassesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.suvash.betterclasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterclassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+		return;
+	}
+
+}
