d51f0c5b9f46f3bc8853c3d8f92d86d07509fb3b
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-20 21:28:35 +0600

    Add UnauthorizedResponseDto.java file

---
+++ src/main/java/com/suvash/betterclasses/dto/response/UnauthorizedResponseDto.java
@@ -0,0 +1,14 @@
+package com.suvash.betterclasses.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UnauthorizedResponseDto {
+    private String message;
+
+    public UnauthorizedResponseDto(String message) {
+        this.message = message;
+    }
+}
