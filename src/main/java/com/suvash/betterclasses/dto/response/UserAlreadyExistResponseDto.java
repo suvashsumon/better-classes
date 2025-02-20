cc3066a5e99dd2b91f7a402d75f1aa6c56bba28e
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-20 20:52:14 +0600

    Implement UserAlreadyExistResponseDto.java file

---
+++ src/main/java/com/suvash/betterclasses/dto/response/UserAlreadyExistResponseDto.java
@@ -0,0 +1,13 @@
+package com.suvash.betterclasses.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserAlreadyExistResponseDto {
+    private String message;
+    public UserAlreadyExistResponseDto(String message) {
+        this.message = message;
+    }
+}
