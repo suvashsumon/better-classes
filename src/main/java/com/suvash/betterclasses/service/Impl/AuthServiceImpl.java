1798482fe21296b974620715338dfaa8b3f268b3
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-02-20 21:28:44 +0600

    Refactor login service for showing correct error message

--- src/main/java/com/suvash/betterclasses/service/Impl/AuthServiceImpl.java
+++ src/main/java/com/suvash/betterclasses/service/Impl/AuthServiceImpl.java
@@ -6,13 +6,13 @@ import com.suvash.betterclasses.config.JwtTokenProvider;
 import com.suvash.betterclasses.dto.AuthResponseDto;
 import com.suvash.betterclasses.dto.LoginDto;
 import com.suvash.betterclasses.dto.RegisterDto;
+import com.suvash.betterclasses.dto.response.UnauthorizedResponseDto;
 import com.suvash.betterclasses.dto.response.UserAlreadyExistResponseDto;
 import com.suvash.betterclasses.model.Role;
 import com.suvash.betterclasses.model.User;
 import com.suvash.betterclasses.repository.RoleRepository;
 import com.suvash.betterclasses.repository.UserRepository;
 import com.suvash.betterclasses.service.AuthService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,27 +43,36 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public ResponseEntity<?> login(LoginDto loginDto) {
 
-        // 01 - AuthenticationManager is used to authenticate the user
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                loginDto.getUsername(),
-                loginDto.getPassword()
-        ));
+        try
+        {
+            // 01 - AuthenticationManager is used to authenticate the user
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    loginDto.getUsername(),
+                    loginDto.getPassword()
+            ));
 
         /* 02 - SecurityContextHolder is used to allows the rest of the application to know
         that the user is authenticated and can use user data from Authentication object */
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        // 03 - Generate the token based on username and secret key
-        String token = jwtTokenProvider.generateToken(authentication);
+            // 03 - Generate the token based on username and secret key
+            String token = jwtTokenProvider.generateToken(authentication);
 
-        AuthResponseDto authResponseDto = new AuthResponseDto();
-        authResponseDto.setAccessToken(token);
+            AuthResponseDto authResponseDto = new AuthResponseDto();
+            authResponseDto.setAccessToken(token);
 
-        // 04 - Return the token to controller
-        return new ResponseEntity<>(
-                new CommonSuccessResponse<AuthResponseDto>(200, authResponseDto),
-                HttpStatus.OK
-        );
+            // 04 - Return the token to controller
+            return new ResponseEntity<>(
+                    new CommonSuccessResponse<>(200, authResponseDto),
+                    HttpStatus.OK
+            );
+        } catch (Exception e)
+        {
+            return new ResponseEntity<>(
+                    new CommonSuccessResponse<>(401, new UnauthorizedResponseDto("Invalid username or password!")),
+                    HttpStatus.UNAUTHORIZED
+            );
+        }
     }
 
     @Override
@@ -71,7 +80,7 @@ public class AuthServiceImpl implements AuthService {
         if (userRepository.existsByUsername(registerDto.getUsername())) {
             UserAlreadyExistResponseDto userAlreadyExistDto = new UserAlreadyExistResponseDto("Username already exist.");
             return new ResponseEntity<>(
-                    new CommonErrorResponse<UserAlreadyExistResponseDto>(409, userAlreadyExistDto),
+                    new CommonErrorResponse<>(409, userAlreadyExistDto),
                     HttpStatus.CONFLICT
             );
         }
@@ -79,7 +88,7 @@ public class AuthServiceImpl implements AuthService {
         if (userRepository.existsByEmail(registerDto.getEmail())) {
             UserAlreadyExistResponseDto userAlreadyExistDto = new UserAlreadyExistResponseDto("Email already exist.");
             return new ResponseEntity<>(
-                    new CommonErrorResponse<UserAlreadyExistResponseDto>(409, userAlreadyExistDto),
+                    new CommonErrorResponse<>(409, userAlreadyExistDto),
                     HttpStatus.CONFLICT
             );
         }
