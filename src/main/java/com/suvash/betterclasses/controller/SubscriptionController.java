324b32432cd361ec3a6d4d0351aa49ff7cc63f4b
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-03-18 22:54:53 +0600

    Add subscription controller

---
+++ src/main/java/com/suvash/betterclasses/controller/SubscriptionController.java
@@ -0,0 +1,15 @@
+package com.suvash.betterclasses.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subscription")
+public class SubscriptionController {
+    @PostMapping("/getPaymentUrl")
+    public String getPaymentUrl() {
+        // Logic to generate payment URL goes here
+            return "https://example.com/payment/url";
+    }
+}
