7014c0002229352ca3a1467280820c80cd3b70ad
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-03-19 02:03:52 +0600

    Add CheckoutRequestDto file

---
+++ src/main/java/com/suvash/betterclasses/dto/request/CheckoutRequestDto.java
@@ -0,0 +1,16 @@
+package com.suvash.betterclasses.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckoutRequestDto {
+  private Long amount;
+  private String currency;
+  private Long quantity;
+  private String name;
+  private String successUrl;
+  private String cancelUrl;
+}
