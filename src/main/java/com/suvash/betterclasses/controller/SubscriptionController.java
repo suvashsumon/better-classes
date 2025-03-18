6e3c7874de33503c280911e874b45233ee60a447
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-03-19 02:08:48 +0600

    Create checkout endpoint

--- src/main/java/com/suvash/betterclasses/controller/SubscriptionController.java
+++ src/main/java/com/suvash/betterclasses/controller/SubscriptionController.java
@@ -1,15 +1,28 @@
 package com.suvash.betterclasses.controller;
 
+import com.suvash.betterclasses.dto.request.CheckoutRequestDto;
+import com.suvash.betterclasses.dto.response.CheckoutResponseDto;
+import com.suvash.betterclasses.service.StripeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/subscription")
 public class SubscriptionController {
-  @PostMapping("/getPaymentUrl")
-  public String getPaymentUrl() {
-    // Logic to generate payment URL goes here
-    return "https://example.com/payment/url";
+  private StripeService stripeService;
+
+  public SubscriptionController(StripeService stripeService) {
+    this.stripeService = stripeService;
+  }
+
+  @PostMapping("/checkout")
+  public ResponseEntity<CheckoutResponseDto> getPaymentUrl(
+      @RequestBody CheckoutRequestDto checkoutRequestDto) {
+    CheckoutResponseDto checkoutResponseDto = stripeService.getSessionUrl(checkoutRequestDto);
+    return ResponseEntity.status(HttpStatus.OK).body(checkoutResponseDto);
   }
 }
