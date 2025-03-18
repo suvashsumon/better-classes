f8bc62901d25feaa6e2c3b4fcf3e5b7826387d44
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-03-19 02:05:43 +0600

    Add CheckoutResponseDto file

---
+++ src/main/java/com/suvash/betterclasses/dto/response/CheckoutResponseDto.java
@@ -0,0 +1,15 @@
+package com.suvash.betterclasses.dto.response;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CheckoutResponseDto {
+  private String status;
+  private String message;
+  private String sessionId;
+  private String sessionUrl;
+}
