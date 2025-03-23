0adf6d269209b5832fa6024e35d5bfe4574849b5
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2025-03-24 00:45:11 +0600

    Update user for adding column of subscription plan

---
+++ src/main/java/com/suvash/betterclasses/entity/User.java
@@ -0,0 +1,56 @@
+package com.suvash.betterclasses.entity;
+
+import com.suvash.betterclasses.enums.SubscriptionType;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String uuid;
+
+	@Column(nullable = false)
+	private SubscriptionType subscriptionType;
+
+	@Column(nullable = false)
+	private LocalDateTime expirationDate;
+
+	@Column(nullable = false)
+	private boolean isActive;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+	private Set<Role> roles;
+
+	@PrePersist
+	public void prePersist() {
+		this.uuid = UUID.randomUUID().toString();
+		this.isActive = true;
+		this.expirationDate = LocalDateTime.now().plusDays(30);
+		this.subscriptionType = SubscriptionType.TRIAL;
+	}
+}
